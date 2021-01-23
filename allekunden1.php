7bf83e021f7a75d5fbb38d4a3c9cd5cf1dc189c1
enricoKoschel <enricokoschel02@gmail.com>
2021-01-23 17:44:08 +0100

    Allekunden now uses Bootstrap

--- allekunden1.php
+++ allekunden1.php
@@ -1,91 +1,129 @@
 <!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
 
-<center>
-	<head>
-		<meta charset="UTF-8">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+		  crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+			crossorigin="anonymous"></script>
 
-		<!-- CSS only -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-			  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-			  crossorigin="anonymous">
-		<!-- JavaScript Bundle with Popper -->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-				integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-				crossorigin="anonymous"></script>
-	</head>
-	<body>
-		<?php
-			//Datenbankvariable festlegen
-			$mServer = "localhost";
-			$mBenutzer = "USER409427";
-			$mKennwort = "AlarmStufeRot";
-			$mDatenbank = "db_409427_2";
-			//Datenbankverbindung herstellen
-			$dbVerbindung = new mysqli($mServer, $mBenutzer, $mKennwort, $mDatenbank);
-			// Datenbankverbindung hergestellt ?
-			if (mysqli_connect_errno() == 0) {
-				$mSQL = "
-					SELECT distinct
-					kunden.KdNr,
-					kunden.Name,
-					SUM(auftragspositionen.AufMenge * artikel.VkPreis) AS Umsatz
+	<script src="//code.jquery.com/jquery.min.js"></script>
+	<script>
+		$.get("nav.html", function (data) {
+			$("#nav-placeholder").replaceWith(data);
+		});
+	</script>
 
-					FROM kunden
-					INNER JOIN auftragskoepfe
-					INNER JOIN auftragspositionen
-					INNER JOIN artikel
+	<title>Lagerwert</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="nav-placeholder"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<?php
+					$mServer = "localhost";
+					$mBenutzer = "USER409427";
+					$mKennwort = "AlarmStufeRot";
+					$mDatenbank = "db_409427_2";
 
-					WHERE auftragskoepfe.KdNr = kunden.KdNr
-					AND auftragskoepfe.AufNr = auftragspositionen.AufNr
-					AND auftragspositionen.ArtikelNr = artikel.ArtikelNr
+					$dbVerbindung = new mysqli($mServer, $mBenutzer, $mKennwort, $mDatenbank);
 
-					GROUP BY KdNr
+					if (mysqli_connect_errno() == 0) {
+						$mSQL = "
+							SELECT distinct
+							kunden.KdNr,
+							kunden.Name,
+							SUM(auftragspositionen.AufMenge * artikel.VkPreis) AS Umsatz
+							
+							FROM kunden
+							INNER JOIN auftragskoepfe
+							INNER JOIN auftragspositionen
+							INNER JOIN artikel
+							
+							WHERE auftragskoepfe.KdNr = kunden.KdNr
+							AND auftragskoepfe.AufNr = auftragspositionen.AufNr
+							AND auftragspositionen.ArtikelNr = artikel.ArtikelNr
+							
+							GROUP BY KdNr
+							
+							UNION
+							
+							SELECT distinct
+							kunden.KdNr,
+							kunden.Name,
+							0 AS Umsatz
+							
+							FROM kunden
+							
+							WHERE KdNr NOT IN (
+							SELECT distinct
+							kunden.KdNr
+							
+							FROM kunden
+							INNER JOIN auftragskoepfe
+							INNER JOIN auftragspositionen
+							INNER JOIN artikel
+							
+							WHERE auftragskoepfe.KdNr = kunden.KdNr
+							AND auftragskoepfe.AufNr = auftragspositionen.AufNr
+							AND auftragspositionen.ArtikelNr = artikel.ArtikelNr
+							)
+		
+							ORDER BY Umsatz DESC
+						";
 
-					ORDER BY Umsatz DESC
-				";
+						$abfrageErgebnis = $dbVerbindung->query($mSQL);
 
-				$abfrageErgebnis = $dbVerbindung->query($mSQL);
+						if ($abfrageErgebnis->num_rows == 0) {
+							echo "<div class='alert alert-info'>Keine Kunden vorhanden!</div>";
+						} else {
+							echo("
+								<table class='table'>
+									<tr>
+										<th>Kunden Nummer</th>
+										<th>Name</th>
+										<th>Umsatz</th>
+									</tr>
+							");
 
-				if ($abfrageErgebnis->num_rows == 0) {
-					echo "<h2>Keine Kunden vorhanden!</h2>";
-				} else {
-					echo("
-						<table border =\"1\">
-							<tr>
-								<th>Kunden Nummer</th>
-								<th>Name</th>
-								<th>Umsatz</th>
-							</tr>
-					");
+							while ($kunde = $abfrageErgebnis->fetch_object()) {
+								$kundenNummer = $kunde->KdNr;
+								$name = utf8_encode($kunde->Name);
+								$umsatz = number_format($kunde->Umsatz, 2, ",", ".");
 
-					while ($kunde = $abfrageErgebnis->fetch_object()) {
-						$kundenNummer = $kunde->KdNr;
-						$name = utf8_encode($kunde->Name);
-						$umsatz = $kunde->Umsatz;
+								echo("
+									<tr>
+										<td>" . $kundenNummer . "</td>
+										<td>" . $name . "</td>
+										<td>" . $umsatz . "</td>
+									</tr>
+								");
+							}
 
-						echo("
-							<tr>
-								<td>" . $kundenNummer . "</td>
-								<td>" . $name . "</td>
-								<td>" . $umsatz . "</td>
-							</tr>
-						");
+							echo("</table>");
+
+							$abfrageErgebnis->close();
+							$dbVerbindung->close();
+						}
+					} else {
+						echo "<div class=\"alert alert-danger\" role=\"alert\">";
+						echo "<h2>Keine Datenbankverbindung</h2>";
+						echo "<p>Fehler: ", mysqli_connect_error(), "</p>";
+						echo "</div>";
 					}
-
-					echo("</table>");
-
-					// Ergebnistabellenobjekt und Datenbankverbindung schließen
-					$abfrageErgebnis->close();
-					$dbVerbindung->close();
-				}
-			} else {
-				echo "<div class=\"alert alert-danger\" role=\"alert\">";
-				echo "<h2>Keine Datenbankverbindung</h2>";
-				echo "<p>Fehler: ", mysqli_connect_error(), "</p>";
-				echo "</div>";
-			}
-
-		?>
-		<a href="index.html"><h1>Startseite</h1></a>
-	</body>
-</center>
+				?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
