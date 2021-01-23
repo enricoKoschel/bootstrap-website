380038c6b0e1b972e6ca06a4d46a3adf28afe5f1
enricoKoschel <enricokoschel02@gmail.com>
2021-01-23 18:45:58 +0100

    Nachrichten now uses Bootstrap

--- erledigen1.php
+++ erledigen1.php
@@ -1,53 +1,67 @@
 <!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
 
-<center>
-	<head>
-		<meta charset="UTF-8">
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+			crossorigin="anonymous"></script>
 
-		<!-- CSS only -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-			  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-			  crossorigin="anonymous">
-		<!-- JavaScript Bundle with Popper -->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-				integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-				crossorigin="anonymous"></script>
-	</head>
-	<body>
-		<?php
-			//Datenbankvariable festlegen
-			$mServer = "localhost";
-			$mBenutzer = "USER409427";
-			$mKennwort = "AlarmStufeRot";
-			$mDatenbank = "db_409427_2";
-			//Datenbankverbindung herstellen
-			$dbVerbindung = new mysqli($mServer, $mBenutzer, $mKennwort, $mDatenbank);
-			// Datenbankverbindung hergestellt ?
-			if (mysqli_connect_errno() == 0) {
-				// Datenübernahme aus der Form
-				$nachrichtenNummer = $_POST['nachrichtenNummer'];
-				// SQL-Anweisung erstellen
+	<script src="//code.jquery.com/jquery.min.js"></script>
+	<script>
+		$.get("nav.html", function (data) {
+			$("#nav-placeholder").replaceWith(data);
+		});
+	</script>
 
-				$mSQL = "
-					UPDATE nachrichten
-					SET erledigt = true
-					WHERE NachrichtNr = '$nachrichtenNummer'
-				";
+	<title>Nachrichten</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="nav-placeholder"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<?php
+					$mServer = "localhost";
+					$mBenutzer = "USER409427";
+					$mKennwort = "AlarmStufeRot";
+					$mDatenbank = "db_409427_2";
 
-				$abfrageErgebnis = $dbVerbindung->query($mSQL);
+					$dbVerbindung = new mysqli($mServer, $mBenutzer, $mKennwort, $mDatenbank);
 
-				if (mysqli_affected_rows($dbVerbindung) > 0) {
-					echo("<h2>Nachricht wurde erledigt!</h2>");
-				} else {
-					echo("<h2>Nachricht nicht vorhanden oder bereits erledigt!</h2>");
-				}
-			} else {
-				echo "<div class=\"alert alert-danger\" role=\"alert\">";
-				echo "<h2>Keine Datenbankverbindung</h2>";
-				echo "<p>Fehler: ", mysqli_connect_error(), "</p>";
-				echo "</div>";
-			}
-		?>
-		<a href="index.html"><h1>Startseite</h1></a>
-	</body>
-</center>
+					if (mysqli_connect_errno() == 0) {
+						$nachrichtenNummer = $_POST['nachrichtenNummer'];
+
+						$mSQL = "
+							UPDATE nachrichten
+							SET erledigt = true
+							WHERE NachrichtNr = '$nachrichtenNummer'
+						";
+
+						$abfrageErgebnis = $dbVerbindung->query($mSQL);
+
+						if (mysqli_affected_rows($dbVerbindung) > 0) {
+							echo("<div class='alert alert-success'>Nachricht wurde erledigt!</div>");
+						} else {
+							echo("<div class='alert alert-info'>Nachricht nicht vorhanden oder bereits erledigt!</div>");
+						}
+					} else {
+						echo "<div class='alert alert-danger' role='alert'>";
+						echo "<h2>Keine Datenbankverbindung</h2>";
+						echo "<p>Fehler: ", mysqli_connect_error(), "</p>";
+						echo "</div>";
+					}
+				?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- nachrichtenAnzeige1.php
+++ nachrichtenAnzeige1.php
@@ -1,92 +1,105 @@
-<center>
-	<head>
-		<!-- CSS only -->
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-		<!-- JavaScript Bundle with Popper -->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-	</head>
-	<body>
-		<?php
-			//Datenbankvariable festlegen
-			$mServer="localhost";
-			$mBenutzer = "USER409427";
-			$mKennwort = "AlarmStufeRot";
-			$mDatenbank = "db_409427_2";
-			//Datenbankverbindung herstellen
-			$dbVerbindung = new mysqli($mServer, $mBenutzer, $mKennwort, $mDatenbank);
-			// Datenbankverbindung hergestellt ?
-			if(mysqli_connect_errno() == 0)
-			{  
-				// SQL-Anweisung erstellen
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">
 
-				$mSQL = "
-					SELECT * FROM nachrichten
-				";
-				
-				$abfrageErgebnis = $dbVerbindung->query($mSQL);
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+		  integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+			crossorigin="anonymous"></script>
 
-				if($abfrageErgebnis->num_rows == 0){
-					echo("<h2>Keine Nachrichten vorhanden!</h2>");
-				}
-				else{
-					echo("
-						<table border =\"1\">
-							<tr>
-								<th>NachrichtNr</th>
-								<th>KdNr</th>
-								<th>EMail</th>
-								<th>Nachricht</th>
-								<th>Erledigt</th>
-							</tr>
-					");
-					
-					while ($nachricht = $abfrageErgebnis->fetch_object())			 				  
-					{ 
-						$nachrichtNr = $nachricht->NachrichtNr;
-						$kundenNummer = $nachricht->KdNr;
-						$email = $nachricht->EMail;
-						$nachrichtenText = utf8_encode($nachricht->Nachricht);						
-						$erledigt = $nachricht->erledigt ? "Ja" : "Nein";
-						
-						
-						echo("
-							<tr>
-								<td>". $nachrichtNr ."</td>
-								<td>". $kundenNummer ."</td>
-								<td>". $email ."</td>
-								<td>". $nachrichtenText ."</td>
-								<td>". $erledigt ."</td>
-							</tr>
+	<script src="//code.jquery.com/jquery.min.js"></script>
+	<script>
+		$.get("nav.html", function (data) {
+			$("#nav-placeholder").replaceWith(data);
+		});
+	</script>
+
+	<title>Nachrichten</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div id="nav-placeholder"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<?php
+					$mServer = "localhost";
+					$mBenutzer = "USER409427";
+					$mKennwort = "AlarmStufeRot";
+					$mDatenbank = "db_409427_2";
+
+					$dbVerbindung = new mysqli($mServer, $mBenutzer, $mKennwort, $mDatenbank);
+
+					if (mysqli_connect_errno() == 0) {
+						$mSQL = "SELECT * FROM nachrichten";
+
+						$abfrageErgebnis = $dbVerbindung->query($mSQL);
+
+						if ($abfrageErgebnis->num_rows == 0) {
+							echo("<div class='alert alert-info'>Keine Nachrichten vorhanden!</>");
+						} else {
+							echo("
+							<table class='table'>
+								<tr>
+									<th>NachrichtNr</th>
+									<th>KdNr</th>
+									<th>EMail</th>
+									<th>Nachricht</th>
+									<th>Erledigt</th>
+								</tr>
 						");
+
+							while ($nachricht = $abfrageErgebnis->fetch_object()) {
+								$nachrichtNr = $nachricht->NachrichtNr;
+								$kundenNummer = $nachricht->KdNr;
+								$email = utf8_encode($nachricht->EMail);
+								$nachrichtenText = utf8_encode($nachricht->Nachricht);
+								$erledigt = $nachricht->erledigt ? "Ja" : "Nein";
+
+
+								echo("
+								<tr>
+									<td>" . $nachrichtNr . "</td>
+									<td>" . $kundenNummer . "</td>
+									<td>" . $email . "</td>
+									<td>" . $nachrichtenText . "</td>
+									<td>" . $erledigt . "</td>
+								</tr>
+							");
+							}
+
+							echo("</table>");
+
+							echo("
+							<form action='erledigen1.php' method='post'>
+								<h3>Nachricht erledigen</h3>
+								<label class='form-label'>Nachrichten Nummer:
+									<input type='text' name='nachrichtenNummer' class='form-control' placeholder='Nachrichten Nummer'/>
+								</label>
+								<br>
+								<button type='submit' class='btn btn-primary'>Senden</button>
+							</form>"
+							);
+
+							$abfrageErgebnis->close();
+							$dbVerbindung->close();
+						}
+					} else {
+						echo "<div class='alert alert-danger' role='alert'>";
+						echo "<h2>Keine Datenbankverbindung</h2>";
+						echo "<p>Fehler: ", mysqli_connect_error(), "</p>";
+						echo "</div>";
 					}
-					
-					echo("</table>");
-					
-					echo("
-					<form action=\"erledigen1.php\" method=\"post\">
-						<p>
-							<h3>Nachricht erledigen:</h3>
-							<input type=\"text\" name=\"nachrichtenNummer\" placeholder=\"Nachrichten Nummer\"/>
-						</p>
-						<p><input type=\"submit\" name=\"\" value=\"Senden\"/></p>
-					</form>"
-					);
-					
-					// Ergebnistabellenobjekt und Datenbankverbindung schließen
-					$abfrageErgebnis->close();
-					$dbVerbindung->close();
-				}
-			}
-			else
-			{ 
-				echo "<div class=\"alert alert-danger\" role=\"alert\">";
-				echo "<h2>Keine Datenbankverbindung</h2>";
-				echo "<p>Fehler: ", mysqli_connect_error(), "</p>";
-				echo "</div>";
-			}
-			
-		end:
-		?>
-		<a href="index.html"><h1>Startseite</h1></a>
-	</body>
-</center>
+				?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
